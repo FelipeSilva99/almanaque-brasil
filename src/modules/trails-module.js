ccf63a279b76d2d42d3c8d4bc4298fa850dbd6b3
Evymendes <evelyn.laurindo@gmail.com>
2021-06-01 10:02:03 -0300

    test if corrected the build

--- src/modules/trails-module.js
+++ src/modules/trails-module.js
@@ -7,7 +7,7 @@ const initialState = {
 };
 
 // Reducer
-export default function(state = initialState, action) {
+export default function foo(state = initialState, action) {
   switch (action.type) {
     case CREATE_NEW_TRAILS:
       return Object.assign({}, state, {

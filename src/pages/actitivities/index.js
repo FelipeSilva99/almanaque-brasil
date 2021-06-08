8f332cac624a9e480664e50a1ac8dddbb1da8b26
Evymendes <evelyn.laurindo@gmail.com>
2021-06-08 15:28:06 -0300

    rendering what is what is

--- src/pages/actitivities/index.js
+++ src/pages/actitivities/index.js
@@ -1,8 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getActivitiesThunk } from '../../dataflow/thunks/activities-thunk'
 import * as Styles from './styles'
+
+import styled from 'styled-components';
+
+//Components
+import Header from '../../components/header/index';
+import TrailsWhatIs from '../../components/trails/whatIsWhatIs';
+import Footer from '../../components/footer/index';
 
 const mapDispatchToProps = dispatch => ({
   getActivities: (trailId) => dispatch(getActivitiesThunk(trailId))
@@ -12,12 +19,31 @@ const mapStateToProps = state => ({
   activities: state.activities
 })
 
+// Styles
+const Container = styled.div`
+  margin: auto;
+  width: 90vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
 const Acitivities = (props) => {
   const { trailId } = useParams();
+  const [nextQuestion, setNextQuestion] = useState(0);
+  const [isAnswer, setIsAnswer] = useState('disco');
 
   useEffect(() => {
     props.getActivities(trailId)
-  }, [])
+  }, []);
+
+  let listLetter = ['disco', 'chuva', 'rua'];
+
+  const handleNextQuestion = () => {
+    setNextQuestion(nextQuestion + 1);
+    setIsAnswer(listLetter[nextQuestion + 1])
+  };
 
   const returnActivities = (activities) => {
     return activities.map((activitie, key) => {
@@ -30,17 +56,17 @@ const Acitivities = (props) => {
   }
 
   return (
-    <>
-      <h1>Hello</h1>
-      <h1>{`Activities trilha ${trailId}`}</h1>
-      <br/>
-      <br/>
-      {
-        props.activities && props.activities.data
-        ? returnActivities(props.activities.data)
-        : <p>carregando...</p>
-      }
-    </>
+    <Container>
+      <Header />
+      <TrailsWhatIs
+        renderQuestion={nextQuestion}
+        isAnswer={isAnswer}
+      />
+      <Footer
+        // handleCleanAnswer={handleCleanAnswer}
+        handleNextQuestion={handleNextQuestion}
+      />
+    </Container>
   );
 }
 

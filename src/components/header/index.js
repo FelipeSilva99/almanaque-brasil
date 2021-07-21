dbdcb86546503cec18399abc39dd5220fbf10b6d
Evymendes <evelyn.laurindo@gmail.com>
2021-07-21 12:01:47 -0300

    starting the screen IfTurnsOn

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,6 +8,9 @@ import iconBack from '../../images/icons/iconBack.svg';
 
 // Styles
 const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100vw;
   /* height: 4rem; */
   padding: 1rem 1rem;

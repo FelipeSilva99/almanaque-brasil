348a2440aadbbcd9f8e0eda3adfd2a4766b3440f
mateusisrael <mateuss.i.fernandes@gmail.com>
2021-07-31 19:48:41 -0300

    saving work

--- src/pages/activities/activitiesList.js
+++ src/pages/activities/activitiesList.js
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
+//Components
+import ActivitieIcon from '../../components/trail/activitieIcon'
+
 const mapStateToProps = state => ({
   activities: state.trails,
   selectedTrails: state.trails.selectedTrails,
 })
 
 // Styles
+
 const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Trail = styled.div`
+  display: flex;
+  max-width: 375px;
   background-color: #fff;
   overflow: hidden;
   width: 100vw;
@@ -18,18 +29,62 @@ const Container = styled.div`
   box-sizing: border-box;
 `;
 
-const Content = styled.button`
-  margin: 1.5rem;
-  width: ${props => props.type === 'origem-da-expressao' || props.type === 'eureka' ? '30px' : '56px'};
-  height: ${props => props.type === 'origem-da-expressao' || props.type === 'eureka' ? '30px' : '56px'};
-  background-color: ${props => props.type === 'origem-da-expressao' || props.type === 'eureka' ? '#cfa151' : '#f8cc80'};
-  border: ${props => props.type === 'origem-da-expressao' || props.type === 'eureka' ? '2px solid #956517' : '2px solid #cfa151'};
+const ActivitiesRow = styled.div`
+  display: flex;
+  /* flex-direction: row-reverse; */
+  justify-content: center;
+`;
+
+
+const LineToRight = styled.div`
+  position: relative;
+  left: 33px;
+  right: 33px;
+  transform: rotate(230deg);
+  width: 150px;
+  height: 150px;
+  border: solid 5px #000;
+  border-color: #000 #000 transparent transparent;
   border-radius: 50%;
-  cursor: pointer;
+  bottom: 58px;
+  z-index: 2;
+  /* background-color: gainsboro; */
+`;
+
+const LineStraight = styled.div`
+  position: relative;
+  left: 33px;
+  right: 33px;
+  transform: rotate(230deg);
+  width: 150px;
+  height: 150px;
+  border: solid 5px #000;
+  border-color: #000 #000 transparent transparent;
+  border-radius: 50%;
+  bottom: 58px;
+  z-index: 2;
+  /* background-color: gainsboro; */
+`;
+
+const LineToleft = styled.div`
+  position: relative;
+  left: 33px;
+  right: 33px;
+  transform: rotate(230deg);
+  width: 150px;
+  height: 150px;
+  border: solid 5px #000;
+  border-color: #000 #000 transparent transparent;
+  border-radius: 50%;
+  bottom: 58px;
+  z-index: 2;
+  /* background-color: gainsboro; */
 `;
 
 const Activities = (props) => {
   const [activities, setActivities] = useState(null);
+  // const [ignoreItems, setIgnoreItems] = useState(null)
+  const [ignoreItem, setIgnoreItem] = useState(false)
 
   useEffect(() => {
     const trail = props.selectedTrails;
@@ -46,17 +101,70 @@ const Activities = (props) => {
     }
   }
 
+
   const hasNextActivitie = () => {
     return true
   }
 
+
+  const renderActivities = () => {
+    let nextItemIsSingular = true;
+    return activities.map((item, index, array) => {
+      if(nextItemIsSingular) {
+        nextItemIsSingular = false
+        return(
+          <>
+            <ActivitiesRow key={index}>
+              <ActivitieIcon
+              item={item}
+              itemValue={index}
+              onClick={() => handlerNextActivitie(index)}
+              history={props}
+              >{index}</ActivitieIcon>
+            </ActivitiesRow>
+          </>
+        )
+      } else {
+        if((index+1) % 3 === 0) {
+          console.log(index+1, 'é multiplo de 3')
+          nextItemIsSingular = true
+          return
+        }
+        else {
+          return (
+            <ActivitiesRow key={index}>
+              <ActivitieIcon
+                item={item}
+                itemValue={index}
+                lineTo={'straight'}
+                onClick={() => handlerNextActivitie(index)}
+                history={props}
+                >{index}</ActivitieIcon>
+                {/* <h1>Mais um</h1> */}
+
+              <ActivitieIcon
+                item={array[index+1]}
+                itemValue={index+1}
+                lineTo={'left'}
+                onClick={() => handlerNextActivitie(index+1)}
+                history={props}
+                >{index+1}</ActivitieIcon>
+            </ActivitiesRow>
+          )
+        }
+      }
+    })
+  } 
+  
   return (
     <Container>
-      {
-        activities && activities.length > 0
-          ? activities.map((item, index) => <Content key={index} type={item.type} onClick={() => handlerNextActivitie(index)} history={props}/>)
-          : <h1>Carregando</h1>
-      }
+      <Trail>
+        {
+          activities && activities.length > 0
+            ? renderActivities()
+            : <h1>Carregando</h1>
+        }
+      </Trail>
     </Container>
   );
 }

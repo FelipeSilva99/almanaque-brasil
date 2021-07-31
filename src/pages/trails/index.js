0a3770b2746c0db1bd9f335d17559c3d15efe817
mateusisrael <mateuss.i.fernandes@gmail.com>
2021-07-31 14:37:15 -0300

    removing styles file from src/trails

--- src/pages/trails/index.js
+++ src/pages/trails/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
-import {
-  Card,
-  Header,
-  Box,
-  Row
-} from './styles';
 
 //Redux
 import {
@@ -21,6 +16,41 @@ const mapDispatchToProps = dispatch => ({
     dispatch(selectedTrails(info));
   },
 });
+
+export const Card = styled.button`
+  min-height: 150px;
+  margin: 10px;
+  border-radius: 25px;
+  padding: 25px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  > h1,h2 {
+    text-decoration: none;
+  }
+  background-color: #fff;s
+
+  &:hover{
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
+`;
+
+export const Box = styled.div`
+  min-height: 100vh;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 35px;
+  height: 10vh;
+  >h1{
+    color: #F99D07;
+  }
+`;
 
 const Trails = (props) => {
 

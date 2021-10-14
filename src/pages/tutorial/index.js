50f7de96833a6079cba149b3c37e7eff7eb11a09
liviavieira <vieira.liviaaa@gmail.com>
2021-10-14 17:43:00 -0300

    add route to tutorial menu

---
+++ src/pages/tutorial/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles";
+
+export default function Tutorial() {
+  return (
+    <S.Container>
+      <h1>Tutorial</h1>
+    </S.Container>
+  )
+}

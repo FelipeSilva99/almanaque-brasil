e08d72fa00dff787e0c71e948361235630d91fbc
mateusisrael <mateuss.i.fernandes@gmail.com>
2021-10-14 17:23:33 -0300

    [AL-543] add click in chest category

--- src/pages/trunk/index.js
+++ src/pages/trunk/index.js
@@ -32,6 +32,7 @@ const ContentTitle = styled.div`
   margin: 1.5rem 0;
   display: flex;
   justify-content: space-between;
+  cursor: pointer;
 `;
 
 const Title = styled.h1`
@@ -95,22 +96,22 @@ const Trunk = (props) => {
 
     return (
       <>
-        <ContentTitle>
+        <ContentTitle  onClick={() => handleModal(title)}>
           <Title>
             {title}
           </Title>
-          <IconModal src={arrow} alt='Seta' isOpen={isModal} onClick={() => handleModal(title)} />
+          <IconModal src={arrow} alt='Seta' isOpen={isModal} />
         </ContentTitle>
         {isModal &&
-          data.filter(item => item.category === title).map(i => (
+          data.filter((item) => item.category === title).map(i => (
             renderOptions(i)
           ))}
       </>
     )
   }
 
-  const renderOptions = (item) => (
-    <ContentText onClick={() => handleInfoModal(item)}>
+  const renderOptions = (item, optionId) => (
+    <ContentText key={optionId} onClick={() => handleInfoModal(item)}>
       <Image src={`data:image/jpeg;base64,${item.imageBase64}`} alt='Img' />
       <Text>{item.title}</Text>
     </ContentText>
